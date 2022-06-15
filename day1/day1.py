995bc2f7c68ae0e3c160c658d3c651d42f2673cc
sach1n1 <sachin.s.dominic@gmail.com>
2022-06-15 15:28:28 +0200

    Day1

---
+++ day1/day1.py
@@ -0,0 +1,6 @@
+# 100 Days of Code: Day 1
+
+print("Welcome to the band name generator!")
+city = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+print("Your band name could be " + city + " " + pet_name)
